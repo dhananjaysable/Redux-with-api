52551c669aec69b1e13aac2414a0d22bf9c42207
Dhananjay Sable <dhananjaysable370@gmail.com>
2025-06-18 16:59:59 +0530

    Login api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
-import { User } from '../models/userModel';
+import { User } from '../models/userModel.js';
 
 export const register = async (req, res) => {
     try {
@@ -34,6 +34,48 @@ export const register = async (req, res) => {
         return res.status(201).json({
             success: true,
             message: `User registered successfully.`
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: `Error in register : ${error.message}`
+        })
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "Credentials required!"
+            })
+        }
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "Invalid Credentials!"
+            })
+        }
+        const comparePassword = await bcrypt.compare(password, user.password);
+        if (!comparePassword) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid Credentials!"
+            })
+        }
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+        const cookieOptions = {
+            httpOnly: true,
+            secure: false,
+            maxAge: 1 * 24 * 60 * 60 * 1000,
+            sameSite: "lax"
+        }
+        return res.cookie('access_token', token, cookieOptions).status(200).json({
+            success: true,
+            message: `Welcome ${user.name.split(" ")[0]}`
         })
     } catch (error) {
         return res.status(500).json({
